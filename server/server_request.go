4d6abebcc9c89b336353e5e08d32c676259836a1
qianxiaoming <qianxiaoming@piesat.cn>
2020-07-05 17:48:49 +0800

    提交的Job内容经转换后写入Job的数据目录

--- server/server_request.go
+++ server/server_request.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"path/filepath"
 
 	"github.com/qianxiaoming/lightsched/data"
@@ -26,10 +29,19 @@ func (svc *APIServer) requestCreateJob(spec *model.JobSpec) error {
 	// 标记任务调度状态
 	svc.setScheduleFlag()
 
-	// 创建Job需要的目录
+	// 创建Job需要的目录。以下功能如果失败不影响任务的执行，因此仅输出日志并不返回失败。
 	dir := filepath.Join(svc.config.dataPath, job.ID)
 	if err := util.MakeDirAll(dir); err != nil {
 		return err
+	}
+	b, err := json.MarshalIndent(job, "", "    ")
+	if err == nil {
+		err = ioutil.WriteFile(filepath.Join(dir, "job_content.json"), b, 0666)
+		if err != nil {
+			log.Printf("Unable to write job content under %s: %v", dir, err)
+		}
+	} else {
+		log.Printf("Failed to marshal job \"%s\" to JSON: %v", job.Name, err)
 	}
 
 	return nil
