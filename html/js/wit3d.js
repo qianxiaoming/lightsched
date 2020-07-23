2be6f49b19ca26cfb6deeee8beccadc859857d9b
qianxiaoming <qianxiaoming@piesat.cn>
2020-07-23 14:09:21 +0800

    初步实现动态页面

--- html/js/wit3d.js
+++ html/js/wit3d.js
@@ -1,5 +1,19 @@
-function initNodeList(result) {
-    $("p").click(function () {
-        $(this).hide();
-    });
+var nodeTemplate
+
+function initNodeList(result, nodesDIV, tmplName) {
+    nodeTemplate = $(tmplName).detach()
+    for(var n in result){
+        node = nodeTemplate.clone(true)
+        let id = "node"+result[n].name
+        $(node).attr("id", id)
+        $(node).find('div[id^="nodeNameIP"]').text(result[n].name+" ("+result[n].address+")")
+        $(node).find('div[id^="nodeOSName"]').text(result[n].platform.name)
+        var gpusNode = $(node).find('div[id^="nodeGPUs"]')
+        var gpuCount = result[n].resources.gpu.cards
+        for(var g=0; g<gpuCount; g++) {
+            $(gpusNode).append($('<img src="img/gpu.png " height="21px " width="21px "></img>'))
+        }
+        $(node).css("visibility", "visible")
+        $(nodesDIV).append(node)
+    }
 }
